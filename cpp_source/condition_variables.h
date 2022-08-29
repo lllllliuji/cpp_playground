e640d880cf886d495bd1ac015b64cf82b784662c
liuji <1546243245@qq.com>
2022-08-29 11:24:15 +0800

    thread_safe_queue_condtion_variables_fine_grained

--- cpp_source/condition_variables.h
+++ cpp_source/condition_variables.h
@@ -1,17 +1,15 @@
 #ifndef Condition_variables_h_
 #define Condition_variables_h_
 
-#include <mutex>
 #include <condition_variable>
+#include <mutex>
 #include <queue>
 
 std::mutex mut;
 std::queue<data_chunk> data_queue;
 std::condition_variable data_cond;
-void data_preparation_thread()
-{
-    while (more_data_to_prepare())
-    {
+void data_preparation_thread() {
+    while (more_data_to_prepare()) {
         data_chunk const data = prepare_data();
         {
             std::lock_guard<std::mutex> lk(mut);
@@ -20,12 +18,10 @@ void data_preparation_thread()
         data_cond.notify_one();
     }
 }
-void data_processing_thread()
-{
-    while (true)
-    {
+void data_processing_thread() {
+    while (true) {
         std::unique_lock<std::mutex> lk(mut);
-        data_cond.wait(lk, []{ return !data_queue.empty();});
+        data_cond.wait(lk, [] { return !data_queue.empty(); });
         data_chunk data = data_queue.front();
         data_queue.pop();
         lk.unlock();
