406e24663fc1e489a9bf678ab8574d797b61659d
liuji <1546243245@qq.com>
2022-10-07 04:54:02 +0800

    add smarter_pointer_test.cpp

---
+++ cpp_source/smarter_pointer_test.cpp
@@ -0,0 +1,30 @@
+#include <memory>
+#include <iostream>
+class A {
+   public:
+    int a_;
+    A() {}
+    A(int a) : a_(a) {}
+    void print() { std::cout << a_ << " " << std::endl; }
+};
+int main() {
+    std::shared_ptr<A> aPtr;
+    {
+        std::shared_ptr<A> ptr = std::make_shared<A>(15);
+        aPtr = ptr;
+        std::cout << ptr.use_count() << " " << aPtr.use_count() << std::endl;
+    }
+    std::weak_ptr<A> wPtr = aPtr;
+    std::cout << aPtr.use_count() << std::endl;
+    if (!wPtr.expired()) {
+        auto w = wPtr.lock();
+        w->print();
+    }
+    auto bPtr = std::make_shared<A>(25);
+    wPtr = bPtr;
+    if (!wPtr.expired()) {
+        auto w = wPtr.lock();
+        w->print();
+    }
+    return 0;
+}
