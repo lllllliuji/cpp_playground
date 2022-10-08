a8f8fe3fafd5d4bf22c3af5a4dd4ae267a1b3311
liuji <1546243245@qq.com>
2022-10-08 18:18:57 +0800

    add file_test.cpp

---
+++ cpp_source/file_test.cpp
@@ -0,0 +1,10 @@
+#include "base.h"
+int main() {
+    std::ifstream infile("./clock_test.cpp");
+    std::string str;
+    while (infile.good()) {
+        std::getline(infile, str);
+        std::cout << str << std::endl;
+    }
+    return 0;
+}
