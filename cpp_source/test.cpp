7515c484b3bed5cdd8294e802a50946c317472dc
liuji <1546243245@qq.com>
2022-10-31 03:10:03 +0800

    xx

--- cpp_source/test.cpp
+++ cpp_source/test.cpp
@@ -1,6 +1,12 @@
 #include "test.h"
 
 int main() {
-    copy_control_test();
+    std::vector<std::pair<int, double>> vec;
+    for (int i = 0; i < 10; i++) {
+        vec.push_back(std::make_pair(i, 1.0 * i));
+    }
+    long long ans = 0;
+    std::for_each(vec.begin(), vec.end(), [&](const auto& item) { ans += item.first; });
+    std::cout << ans << std::endl;
     return 0;
 }
